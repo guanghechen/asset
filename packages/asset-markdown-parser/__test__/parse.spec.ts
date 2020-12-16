7320124c3be5e14c0f5c72f738c7c7fd9402892b
guanghechen <me.clown.lemon@gmail.com>
2020-12-16 21:51:59 +0800

    :white_check_mark:  test(asset-markdown-parser): update tests

---
+++ packages/asset-markdown-parser/__test__/parse.spec.ts
@@ -0,0 +1,18 @@
+import fs from 'fs-extra'
+import path from 'path'
+import parse from '../src'
+
+
+describe('parser', function () {
+  const caseDir = path.resolve(__dirname, 'case')
+  const cases = fs.readdirSync(caseDir)
+  for (const kase of cases) {
+    const filepath = path.join(caseDir, kase)
+
+    test(path.parse(kase).name, async function () {
+      const content = await fs.readFile(filepath, 'utf-8')
+      const result = parse(content)
+      expect(result).toMatchSnapshot()
+    })
+  }
+})
