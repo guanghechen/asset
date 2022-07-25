beb065e9f19f318c35d5396870088324c3b684a8
guanghechen <me.guanghechen@gmail.com>
2022-07-25 20:45:51 +0800

    :rotating_light:  style: fix lint warnings

--- packages/asset-plugin-file/src/entity.ts
+++ packages/asset-plugin-file/src/entity.ts
@@ -1,4 +1,5 @@
 export const AssetFileType = 'file'
+// eslint-disable-next-line @typescript-eslint/no-redeclare
 export type AssetFileType = typeof AssetFileType
 
 export interface IAssetFileData {
--- packages/asset-plugin-markdown/src/entity.ts
+++ packages/asset-plugin-markdown/src/entity.ts
@@ -1,6 +1,7 @@
 import type { Root } from '@yozora/ast'
 
 export const AssetMarkdownType = 'markdown'
+// eslint-disable-next-line @typescript-eslint/no-redeclare
 export type AssetMarkdownType = typeof AssetMarkdownType
 
 export interface IAssetMarkdownData {
