1358b71cc7d5d0ff322a7130ce3d2a8b9868f7a0
guanghechen <me.guanghechen@gmail.com>
2023-03-11 01:22:11 +0800

    :wrench:  chore: fix test configs

---
+++ jest.config.mjs
@@ -0,0 +1,40 @@
+import { tsMonorepoConfig } from '@guanghechen/jest-config'
+import { resolve } from 'import-meta-resolve'
+import path from 'node:path'
+import url from 'node:url'
+
+export default async function() {
+  const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+  const chalkLocation = url.fileURLToPath(await resolve('chalk', import.meta.url))
+  const baseConfig = await tsMonorepoConfig(__dirname, { useESM: true })
+  const { default: manifest } = await import(path.resolve('package.json'), {
+    assert: { type: 'json' },
+  })
+
+  return {
+    ...baseConfig,
+    coverageProvider: 'babel',
+    coverageThreshold: {
+      global: {
+        branches: 50,
+        functions: 65,
+        lines: 60,
+        statements: 60,
+        ...manifest.jest?.coverageThreshold?.global,
+      },
+    },
+    extensionsToTreatAsEsm: ['.ts', '.mts'],
+    moduleNameMapper: {
+      ...baseConfig.moduleNameMapper,
+      chalk: chalkLocation,
+      '#ansi-styles': path.join(
+        chalkLocation.split('chalk')[0],
+        'chalk/source/vendor/ansi-styles/index.js',
+      ),
+      '#supports-color': path.join(
+        chalkLocation.split('chalk')[0],
+        'chalk/source/vendor/supports-color/index.js',
+      ),
+    },
+  }
+}
