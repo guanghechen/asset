65888ca22a4720d72705b3c84fe7a5e373e6a09f
guanghechen <me.guanghechen@gmail.com>
2024-02-04 20:35:01 +0800

    :bug:  fix: mime.define only allowed on customized instance

--- packages/asset-util/src/mime.ts
+++ packages/asset-util/src/mime.ts
@@ -1,5 +1,11 @@
-import mime from 'mime'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Mime } from 'mime'
+// @ts-expect-error
+import otherTypes from 'mime/types/other.js'
+// @ts-expect-error
+import standardTypes from 'mime/types/standard.js'
 
+export const mime = new Mime(standardTypes, otherTypes)
 mime.define(
   {
     'text/x-c++src': ['cpp'],
@@ -7,5 +13,3 @@ mime.define(
   },
   true,
 )
-
-export { mime }
